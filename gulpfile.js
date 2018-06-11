9dfb78b5e7850cf491a878bbe9708b61a4ae61f7
Dandy.Chen <ro1963855@gmail.com>
2018-06-11 23:03:50 +0800

    add gulp watch to compile scss files and minify
    1.cmd
    >npm install -S node-sass less
    >npm install -S gulp gulp-sass gulp-less
    >npm install -S gulp-concat
    >npm install -S gulp-minify-css
    >npm install -S gulp-rename
    
    2.add .vscode/tasks.json
    
    3.add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+// Sass configuration
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var rename = require("gulp-rename");
+
+gulp.task('sass', function () {
+    gulp.src('./src/assets/stylesheet/scss/**/*.scss')
+        .pipe(sass())
+        .on('error', console.error.bind(console))
+        .pipe(gulp.dest('./src/assets/stylesheet/css/'))
+});
+
+gulp.task('concat', function () {
+    return gulp.src('./src/assets/stylesheet/css/**/*.css')
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('./src/assets/stylesheet/build/'));
+});
+
+gulp.task('minify-css', ['concat'], function () {
+    return gulp.src('./src/assets/stylesheet/build/all.css')
+        .pipe(minifyCSS({
+            keepBreaks: true,
+        }))
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+            path.extname = ".css";
+        }))
+        .pipe(gulp.dest('./src/assets/stylesheet/build/'));
+});
+
+
+gulp.task('default', ['sass', 'minify-css'], function () {
+    gulp.watch('./src/assets/stylesheet/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/assets/stylesheet/css/**/*.css', ['minify-css']);
+});
